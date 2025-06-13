398c5f2dadf0c650443ba2c74948fa3873582a86
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-06-13 17:36:00 +0000

    code added

--- client.py
+++ client.py
@@ -9,3 +9,13 @@ ADDRESS = (SERVER, PORT)
 
 client = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 client.connect(ADDRESS)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    message_length = len(message)
+    send_length = str(message_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+send("Hello, World!")
