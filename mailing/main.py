b171d98ad90d09b96d760798715253d711a56c07
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-06-14 14:38:31 +0000

    code added

--- mailing/main.py
+++ mailing/main.py
@@ -4,18 +4,21 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
+sender_mail = 'signuptemp1247@securemail.org'
+receiver_mail = 'nbqr6@punkproof.com'
+
+server = smtplib.SMTP('smtp.google.com', 587)
 server.ehlo()
 server.starttls()
 
 with open('password.txt', 'r') as p:
     password = p.read()
 
-server.login('code.nischal.tech@gmail.com', password)
+server.login(sender_mail, password)
 
 msg = MIMEMultipart()
 msg['From'] = 'Nischal Paliwal'
-msg['To'] = 'nischalpaliwal@gmail.com'
+msg['To'] = receiver_mail
 msg['Subject'] = 'Just a testing mail!'
 
 with open('body.txt', 'r') as f:
@@ -34,4 +37,4 @@ payload.add_header('Content-Disposition', f"attachment; filename={image_filename
 msg.attach(payload)
 
 text = msg.as_string()
-server.sendmail(msg=text, from_addr='code.nischal.tech@gmail.com', to_addrs='nischalpaliwal@gmail.com')
+server.sendmail(msg=text, from_addr=sender_mail, to_addrs=receiver_mail)
